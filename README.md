1b3480492b54ff4aa62055d60df5d04ef33a863c
bruce mckenzie <bjmckenz@gmail.com>
2023-12-03 09:52:41 -0800

    cleaned up, less bugs

--- README.md
+++ README.md
@@ -4,44 +4,98 @@ A simple clonk server
 
 ## Setup
 
-*(Install Python)*
+ *(Install Python)*
 
 `pip install -r requirements.txt`
 
 ## Execution
 
-`flask --app clonker run`
+`python clonker.py [ -d ] [ -m <dist> ] [ -t <time> ] run`
+
+* `-d` - enable debug to stdout
+* `-m` - use `<dist>` *(meters)* for "close enough" limit
+* `-t` - use `<time>` *(seconds)* for "close enough" time window
+
 
 ## Use
 
-1. Hit `<server>/clonk/name1`
-2. After the window, you'll get back a list *(JSON)* of clients who also clonked within the window.
+1. POST to `<server>/clonk`
 
 ```json
 {
-    "clonks_within_window": [
-        {
-            "at": 1701368861.977858,
-            "username": "aaa"
-        },
-        {
-            "at": 1701368863.5652878,
-            "username": "bbb"
-        }
-    ]
+    "version": "1.0.0",
+    "id": "my name or id",
+    "latitude": 40.000000,
+    "longitude": -40.00000,
+    "altitude": 0.5,
+    "some other data": "banana"
 }
 ```
 
-*(`at` represents server time in ms)*
+* `version` represents the client protocol version and must be `1.0.0`
+* JSON Body must contain an `id` key. This is used to distinguish clients.
+* JSON Body MAY contain any/all keys 'latitude', 'longitude', 'altitude'
+  * lat and lon are in degrees
+  * alt is meters. Altitude can be provided by both or neither events. If it's provided by only one, it will not report a clonk.
+* JSON Body may contain other keys. They will be sent to clonk-mates.
 
-3. You will probably be in the list.
+```json
+{
+    "version": "1.0.0",
+    "id": "your name",
+    "latitude": 40.0,
+    "longitude": -40.0,
+    "altitude": 10,
+    "some other data": "banana"
+}
+```
+
+1. After the window, you'll get back a list *(JSON)* of the client clonks for clients who also clonked (with you) within the window.
+
+`version` represents server version.
+
+```json
+{
+  "version": "1.0.0",
+  "clonks": [
+    {
+      "id": "client 1",
+      "latitude": 40.000002,
+      "longitude": -40.0,
+      "altitude": 0.5,
+      "at": 1701545253.113344,
+      "some other data": "banana"
+    },
+    {
+      "id": "client 2",
+      "latitude": 40.000001,
+      "longitude": -40.0,
+      "altitude": 1.5,
+      "at": 1701545255.066371,
+      "some other data": "raspberry"
+    }
+  ]
+}
+```
+
+1. You will not be in the list.
+
+## What's a clonk?
+
+**Requests within a given distance and time window**
 
 ## Defaults
 
-**Clonk window:** 2 seconds
+**Clonk window:** 0.5 seconds
+
+Why not make this 0.0? Potentially different latencies in opening a connnection to the server. Times are recorded by the server, not reported by the client so skew is not an issue
+
+**Distance between devices:** 1.0 meter
+
+Why not make this 0.0? Inaccuracies in phone position. Locations are fuzzy.
 
 **Settling window:** 20 ms
-*(This compensates for server latency, not network latency)*
+*(connection and network latency, and a bit of server latency)*
 
 # License
 
--- clonker.py
+++ clonker.py
@@ -1,31 +1,125 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from geopy.distance import great_circle
 import time
+import sys
 
 # All times in seconds
 
-app = Flask(__name__)
-
-CLONK_MATCHUP_WINDOW = 2.000
+DEFAULT_CLOSE_ENOUGH_METERS = 1.0
+DEFAULT_TIME_MATCHUP_WINDOW = 0.5
+DEFAULT_IDENTITY_KEY = 'id'
 
 # "Settle" ensures that we'll still be in the list for a bit
-# after the sleep CLONK_MATCHUP_WINDOW
-# impacted by service time for the request
-SETTLE = 0.020
+# after the sleep DEFAULT_TIME_MATCHUP_WINDOW
+# insurance compensating for service time of the request
+SETTLE = 0.005
 
-clonks_within_window = []
+clonks_within_time_window = []
 
-@app.route('/clonk/<username>', methods=['GET'])
-def clonk(username):
-    global clonks_within_window
+# app must be defined and initialized before routes.
+# non-Flask arguments must be parsed before app is initialized.
+if __name__ == '__main__':
 
+    global app
+
+    config = {}
+    config['distance'] = DEFAULT_CLOSE_ENOUGH_METERS
+    config['time'] = DEFAULT_TIME_MATCHUP_WINDOW
+    config['debug'] = False
+
+    if len(sys.argv) > 1 and sys.argv[1] == '-d':
+        config['debug'] = True
+        sys.argv = [sys.argv[0]] + sys.argv[2:]
+
+    if len(sys.argv) > 1 and sys.argv[1] == '-m':
+        config['distance'] = int(sys.argv[2])
+        sys.argv = [sys.argv[0]] + sys.argv[3:]
+
+    if len(sys.argv) > 1 and sys.argv[1] == '-t':
+        config['time'] = int(sys.argv[2])
+        sys.argv = [sys.argv[0]] + sys.argv[3:]
+
+    app = Flask(__name__)
+
+    for key in config:
+        app.config[key] = config[key]
+
+
+@app.route('/clonk', methods=['POST'])
+def clonk():
+    client = request.json
     now = time.time()
 
-    clonks_within_window.append({"username": username, "at": now})
+    if DEFAULT_IDENTITY_KEY not in client:
+        return jsonify({
+            "error": "no identity key",
+            "required": DEFAULT_IDENTITY_KEY
+            }), 400
 
-    time.sleep(CLONK_MATCHUP_WINDOW)
+    my_event = client | {"at": now}
+    if app.config['debug']:
+        print(my_event, file=sys.stderr)
 
-    clonks_within_window = list(filter(
-        lambda x: x["at"] > now - CLONK_MATCHUP_WINDOW + SETTLE,
-        clonks_within_window))
+    global clonks_within_time_window
 
-    return jsonify({"clonks_within_window": clonks_within_window})
+    clonks_within_time_window = \
+        list(filter(lambda x: not same_client(my_event, x),
+                    clonks_within_time_window)) + \
+        [my_event]
+
+    # wait for our peer clonkers
+    time.sleep(app.config['time'])
+
+    clonks_within_time_window = list(filter(
+        lambda x: x["at"] > now - app.config['time'] + SETTLE,
+        clonks_within_time_window))
+
+    if app.config['debug']:
+        print(list(clonks_within_time_window), file=sys.stderr)
+
+    clonking_me = filter(lambda x: are_clonking(my_event, x),
+                         clonks_within_time_window)
+
+    return jsonify({"clonks": list(clonking_me)})
+
+
+def hypotenuse(s1, s2):
+    return (s1**2 + s2**2)**0.5
+
+
+def same_client(clonk1, clonk2):
+    return clonk1[DEFAULT_IDENTITY_KEY] == clonk2[DEFAULT_IDENTITY_KEY]
+
+# "is within time window" is assumed
+def are_clonking(clonk1, clonk2):
+    return not same_client(clonk1, clonk2) and are_close_enough(clonk1, clonk2)
+
+def distance_can_be_calculated(clonk1, clonk2):
+    return 'latitude' in clonk1 and 'latitude' in clonk2 and \
+        'longitude' in clonk1 and 'longitude' in clonk2 and \
+        ('altitude' in clonk1) == ('altitude' in clonk2)
+
+
+def are_close_enough(clonk1, clonk2):
+    return distance_can_be_calculated(clonk1, clonk2) and \
+        distance_between(clonk1, clonk2) < app.config['distance']
+
+
+def distance_between(clonk1, clonk2):
+    sea_level_distance = great_circle(
+        (clonk1['latitude'], clonk1['longitude']),
+        (clonk2['latitude'], clonk2['longitude']),
+    ).meters
+
+    dist = sea_level_distance
+
+    # incorporate altitude (farily valid for << 1km) if it's available
+    if 'altitude' in clonk1:
+        dist = hypotenuse(sea_level_distance,
+                          clonk1['altitude'] - clonk2['altitude'])
+
+    return dist
+
+
+if __name__ == '__main__':
+    app.run()
